b1ec6d1cc649afbfb31e0e2fcf239e57706e303e
LegionDev370 <llegion630@gmail.com>
2025-08-07 16:47:08 +0500

    some code added

--- src/components/steps/step-1.tsx
+++ src/components/steps/step-1.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useRef } from "react";
 import "../../assets/styles/input.css";
+import useSendOtp from "../../hooks/requests/useSendOtp";
 import Input from "../ui/Input";
 import InputMask from "../ui/input-mask";
 import OtpInput from "../ui/otp-input";
+import { toast } from "react-toastify";
 const Step1 = () => {
+  const {
+    mutateAsync,
+    isSuccess: sendOtpSuccess,
+    isError,
+    isPending,
+  } = useSendOtp();
+  const ref = useRef<HTMLInputElement>(null);
+  const handleClick = () => {
+    const phoneNumber = ref.current?.value;
+    mutateAsync(phoneNumber);
+  };
+  useEffect(() => {
+    if (sendOtpSuccess) toast.success(`Sms code sended`);
+  }, [sendOtpSuccess]);
   return (
     <div className="w-full">
-      <form className="flex flex-col gap-y-[31px] mt-[33px]">
-        <InputMask label="Mobile Number" />
-        <OtpInput label="Code from SMS" />
+      <form
+        onSubmit={(event) => event.preventDefault()}
+        className="flex flex-col gap-y-[31px] mt-[33px]"
+      >
+        <InputMask
+          handleClick={handleClick}
+          isLoading={isPending}
+          inputRef={ref}
+          label="Mobile Number"
+        />
+        {sendOtpSuccess && <OtpInput label="Code from SMS" />}
         <Input
           inputClassName="w-full"
           type="email"
--- src/components/ui/input-mask.tsx
+++ src/components/ui/input-mask.tsx
@@ -1,18 +1,23 @@
 import { IMaskInput } from "react-imask";
 import Button from "./Button";
-import Icon from "./Icon";
+import type { RefObject } from "react";
+import Loader from "./Loader";
 
 interface Props {
   label: string;
+  inputRef: RefObject<HTMLInputElement>;
+  handleClick: () => void;
+  isLoading?: boolean;
 }
 
-const InputMask = ({ label }: Props) => {
+const InputMask = ({ label, inputRef, handleClick, isLoading }: Props) => {
   return (
     <>
       <div className="flex flex-col gap-y-2">
         <label className="input-label">{label}</label>
         <div className="flex w-full gap-x-1.5">
           <IMaskInput
+            inputRef={inputRef}
             className="input w-full"
             mask="{+998} 00 000 00 00"
             defaultValue="+998"
@@ -21,10 +26,12 @@ const InputMask = ({ label }: Props) => {
             minLength={17}
           />
           <Button
+            onClick={handleClick}
+            itemType="button"
             variant="small"
             className="flex !py-[5px] items-center gap-x-3"
           >
-            Send
+            {isLoading ? <Loader /> : "Send"}
           </Button>
         </div>
       </div>
