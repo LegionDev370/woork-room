7ee14ccd26b918119bdb6dd37adf5b573da74452
LegionDev370 <llegion630@gmail.com>
2025-09-11 16:29:12 +0500

    some code adde

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 export default defineConfig({
   plugins: [react(), tailwindcss(), nodePolyfills()],
+  preview: {
+    port: 4173,
+    allowedHosts: ["fiver.uz", "www.fiver.uz", "localhost"],
+  },
   server: {
     proxy: {
       "/socket.io": {
